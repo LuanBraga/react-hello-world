2f208f1f702aa28c55c657a793df590273fc8d1f
LuanBraga <luanbraga.lb@gmail.com>
2022-01-13 14:44:50 -0300

    renderizaçao condicional

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './index.css';
 // import reportWebVitals from './reportWebVitals';
 import logo from './logo.svg';
 import './App.css';
-import { render } from '@testing-library/react';
 
 const user = {
   firstName: 'Zé',
@@ -90,12 +89,148 @@ class Toggle extends React.Component {
   }
 }
 
+//statefull component
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.handleLoginClick = this.handleLoginClick.bind(this);
+    // this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick = () => {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick = () => {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if(isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick}/>;
+    }else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+
+        {/* if-Else inline com operador condicional */}
+        <div>
+          The user is<b>{isLoggedIn ? ' currently' : ' not'}</b> logged in.
+        </div>
+      </div>
+    );
+  }
+}
+
+//Variáveis de elemento
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+//renderização condicional
+
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up</h1>;
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+
+  if(isLoggedIn){
+    return <UserGreeting/>
+  }
+  return <GuestGreeting/>
+}
+
+//if inline &&
+function MailBox(props) {
+  const unreadMessages = props.unreadMessages;
+  return (
+    <div>
+      <h1>Hello !</h1>
+      {unreadMessages.length > 0 &&
+        <h2>
+          You Have {unreadMessages.length} unread messages.
+        </h2>
+      }
+    </div>
+  );
+}
+
+// const messages = [];
+const messages = ['React', 'Re: React', 'Re:Re: React'];
+
+//Evitando que um componente seja renderizado
+function WarningBanner(props) {
+  if(!props.warn) {
+    return null;
+  }
+
+  return (
+    <div className="warning">
+      Warning!!
+    </div>
+  );
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showWarning: true};
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState(state => ({
+      showWarning: !state.showWarning
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'hide' : 'Show'}
+        </button>
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
     <div>
       <Clock/>
       <Form/>
       <Toggle/>
+      <LoginControl/>
+      <MailBox unreadMessages={messages} />
+      <Page/>
     </div>
   );
 }
@@ -120,6 +255,9 @@ function Form(props) {
     </form>
   );
 }
+
+
+
 
 
 
